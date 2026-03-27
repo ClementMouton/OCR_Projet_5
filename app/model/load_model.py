2034edd8bb4b15aaa97094a16bfd5d24d2ffacfa
Clement M <mouton.cl@gmail.com>
2026-03-27 12:24:47 +0100

    connexion faite

--- app/model/load_model.py
+++ app/model/load_model.py
@@ -1,7 +1,15 @@
-import joblib
+import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[2]
-MODEL_PATH = Path("artifacts/attrition_pipeline.joblib")
+import joblib
+from huggingface_hub import hf_hub_download
+
+HF_REPO_ID = "ClementMouton/employee-attrition-model"
+
+MODEL_PATH = hf_hub_download(
+    repo_id=HF_REPO_ID,
+    filename="attrition_pipeline.joblib",
+    repo_type="model"
+)
 
 model = joblib.load(MODEL_PATH)
--- app/model/predict.py
+++ app/model/predict.py
@@ -1,13 +1,20 @@
 import json
-from pathlib import Path
 
 import pandas as pd
+from huggingface_hub import hf_hub_download
 
 from app.db.queries import insert_prediction
 from app.model.load_model import model
 
-BASE_DIR = Path(__file__).resolve().parents[2]
-METADATA_PATH = BASE_DIR / "artifacts" / "model_metadata.json"
+
+# 🔗 Repo Hugging Face
+HF_REPO_ID = "ClementMouton/employee-attrition-model"
+
+# 📥 Récupération du metadata depuis HF
+METADATA_PATH = hf_hub_download(
+    repo_id=HF_REPO_ID,
+    filename="model_metadata.json"
+)
 
 with open(METADATA_PATH, "r", encoding="utf-8") as f:
     model_metadata = json.load(f)
@@ -16,17 +23,21 @@ THRESHOLD = model_metadata["threshold"]
 
 
 def predict(data: dict) -> dict:
+    # Transformation en DataFrame
     df = pd.DataFrame([data])
 
+    # Prédiction
     proba = model.predict_proba(df)[0, 1]
     pred = int(proba >= THRESHOLD)
 
+    # 🔥 Enregistrement en base PostgreSQL
     insert_prediction(
         input_data=data,
         prediction=pred,
         probability=float(proba)
     )
 
+    # Retour API
     return {
         "prediction": pred,
         "probability": float(proba)
